01f3205ef0eb71a189a2e1f64046a13a57e005d5
akm <akm2000@gmail.com>
2023-11-26 18:26:53 +0900

    :sparkles: Generating the client

--- connect-web-example/src/App.tsx
+++ connect-web-example/src/App.tsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { createPromiseClient } from "@connectrpc/connect";
+import { createConnectTransport } from "@connectrpc/connect-web";
+
+// Import service definition that you want to connect to.
+import { ElizaService } from "@buf/connectrpc_eliza.connectrpc_es/connectrpc/eliza/v1/eliza_connect";
+
+// The transport defines what type of endpoint we're hitting.
+// In our example we'll be communicating with a Connect endpoint.
+// If your endpoint only supports gRPC-web, make sure to use
+// `createGrpcWebTransport` instead.
+const transport = createConnectTransport({
+  baseUrl: "https://demo.connectrpc.com",
+});
+
+// Here we make the client itself, combining the service
+// definition with the transport.
+const client = createPromiseClient(ElizaService, transport);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +47,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
